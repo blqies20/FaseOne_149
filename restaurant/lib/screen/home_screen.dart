57b5bdadbf891d4ee0edf31400bf531dc4747e67
ACER <ACER@DESKTOP-QH5VR0V>
2024-03-15 21:05:05 +0700

    add home screen

--- restaurant/lib/screen/home_screen.dart
+++ restaurant/lib/screen/home_screen.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required});
+  const HomeScreen({super.key, required this.nama, required this.number});
+
+  final String nama;
+  final String number;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Screen"),
+        title: Text("YUMMIE"),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("hai")],
+          children: [
+            Column(
+              children: <Widget>[
+                Text("Nama : $nama", style: TextStyle(fontSize: 15)),
+                Text("Telepon : $number", style: TextStyle(fontSize: 15)),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Makanan",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            
+          ],
         ),
       ),
     );
